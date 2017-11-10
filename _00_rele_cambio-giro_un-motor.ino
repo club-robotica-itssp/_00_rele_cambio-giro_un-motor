156da24320b3f473e374b864fb5ee73a6351a7d8
mataperros <jdavsg@gmail.com>
2017-11-10 13:20:26 -0600

    Programa y diagrama

---
+++ _00_rele_cambio-giro_un-motor.ino
@@ -0,0 +1,26 @@
+/*  Programa para controlar el giro de un motor
+ *  Utilizando dos relevadores para el cambio
+ *  de polaridad.
+ */
+
+void setup() {
+  // Declarando salidas.
+    pinMode(2, OUTPUT);
+  
+  // Valores iniciales (Giro detenido)
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+}
+
+void loop() {
+  // Activando giro hacia un lado.
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  delay(2000);
+  
+  // Activando giro hacia el lado contrario.
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  delay(2000);
+}
+
